f0f1f4155fd5dd1cfee3b73b5220209f2d3656d1
MV88 <matteo.velludini@gmail.com>
2018-10-14 23:39:45 +0200

    Setting up the project

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/indexTemplate.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+module.exports = {
+    entry: [
+        "./src/app.js"
+    ],
+	output: {
+		path: __dirname,
+        publicPath: '/',
+		filename: "public/build/app.bundle.js"
+	},
+	plugins: [HtmlWebpackPluginConfig],
+    module: {
+    	rules: [
+    		{
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [{
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }]
+        }
+    	]
+  	},
+    devServer: {
+        historyApiFallback: true,
+        proxy: {
+            '/scores': {
+                target: "http://localhost:5000",
+                secure: false,
+                headers: {
+                    host: "localhost"
+                }
+            }
+        }
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
