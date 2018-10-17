ed71029eb15b015ceb3522d417de6b60986683cb
Matteo V <matteo.velludini.geosolutions@gmail.com>
2018-10-17 23:24:43 +0200

    Fixed project setup with babel (#5)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { Route, BrowserRouter } from 'react-router-dom';
-import GridPhone from './components/containers/GridPhone';
+import Dashboard from './components/containers/Dashboard';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
